3d244c739d8363cdaf9fb17121c772546c3b2cb9
Rebecca Everton <rebeccaeverton@Rebeccas-MacBook-Air-2.local>
2026-03-25 22:38:30 +0000

    feat: update LandingPage section order, add stub components

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,33 +1,31 @@
 import { HeroSection } from '../components/landing/HeroSection';
-import { StorySection } from '../components/landing/StorySection';
 import { ProblemSection } from '../components/landing/ProblemSection';
+import { StorySection } from '../components/landing/StorySection';
+import { GapSection } from '../components/landing/GapSection';
 import { ClearMethodSection } from '../components/landing/ClearMethodSection';
+import { ScreenshotsSection } from '../components/landing/ScreenshotsSection';
 import { JourneySection } from '../components/landing/JourneySection';
-import { CalmSection } from '../components/landing/CalmSection';
 import { PricingSection } from '../components/landing/PricingSection';
-import { GuaranteeSection } from '../components/landing/GuaranteeSection';
 import { SocialProofSection } from '../components/landing/SocialProofSection';
 import { FaqSection } from '../components/landing/FaqSection';
 import { FinalCtaSection } from '../components/landing/FinalCtaSection';
 import { LandingFooter } from '../components/landing/LandingFooter';
-import { StickyCtaBar } from '../components/landing/StickyCtaBar';
 
 export function LandingPage() {
   return (
-    <div className="min-h-screen pb-20">
+    <div className="min-h-screen">
       <HeroSection />
-      <StorySection />
       <ProblemSection />
+      <StorySection />
+      <GapSection />
       <ClearMethodSection />
+      <ScreenshotsSection />
       <JourneySection />
-      <CalmSection />
       <PricingSection />
-      <GuaranteeSection />
       <SocialProofSection />
       <FaqSection />
       <FinalCtaSection />
       <LandingFooter />
-      <StickyCtaBar />
     </div>
   );
 }
